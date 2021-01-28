7deb9907562b11349b8aee4313a95d565d683ea0
Bradley Lund <bradjlund@gmail.com>
2021-01-28 11:32:12 +0200

    using multiple props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Hello stinky" image={faker.image.image()}/>
+            <CommentDetail author="Brad" timeAgo="Today at 2:00AM" text="you are" image={faker.image.image()}/>
+            <CommentDetail author="Craig" timeAgo="Yesterday at 6:00PM" text="Poop" image={faker.image.image()}/>
         </div>
     )
 };
